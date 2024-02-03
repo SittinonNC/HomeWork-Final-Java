fe37730116d58d9836b3d48cc764a531f2ecd7b4
nice <nice23615@gmail.com>
2024-02-04 04:36:37 +0700

    FirstVersion

---
+++ bank/BankAccount.java
@@ -0,0 +1,54 @@
+package bank;
+
+import javax.swing.JOptionPane;
+
+public class BankAccount {
+
+    private double balance;
+
+    public BankAccount() {
+        balance = 0;
+    }
+
+    public BankAccount(double startBalance) {
+        balance = startBalance;
+    }
+
+    public BankAccount(String str) {
+        balance = Double.parseDouble(str);
+    }
+
+    public void setBalance(double newBalance) {
+        balance = newBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void deposit(String str) {
+        balance += Double.parseDouble(str);
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            JOptionPane.showMessageDialog(null, "Insufficient funds");
+        }
+    }
+
+    public void withdraw(String str) {
+        double amount = Double.parseDouble(str);
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            JOptionPane.showMessageDialog(null, "Insufficient funds");
+        }
+    }
+
+}
